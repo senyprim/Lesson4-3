d962e60e3d0ea91a51c5919bd4128cd360335374
EVA <seny_ussur@mail.ru>
2019-07-25 06:55:01 +1000

    4-3-4

--- test_product_page.py
+++ test_product_page.py
@@ -1,18 +1,23 @@
+import pytest
 from pages.product_page import ProductPage
 import time
 
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 
-def test_guest_can_add_product_to_cart(browser):
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+def test_guest_can_add_product_to_cart(browser,link):
+
     page=ProductPage(browser, link)
     page.open()
     page.add_to_basket()
-
-
-
-
-
-
-
-
+    page.solve_quiz_and_get_code()
+    assert page.get_product_name() == page.get_added_product_to_basket(), f"Товар со страницы {link} не соответствует товару в корзине после добавления"
+    assert page.get_product_price() == page.get_price_to_basket(),f"Цена со страницы {link} не соответствует цене товара который добавили"
